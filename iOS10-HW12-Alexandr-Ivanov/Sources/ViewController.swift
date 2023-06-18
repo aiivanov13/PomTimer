f417d5609f648796280d943f9a0e8c2ca5f460db
aivanov <alex130896@gmail.com>
2023-06-19 01:56:43 +0400

    Add title label
    Добавил title

--- iOS10-HW12-Alexandr-Ivanov/Sources/ViewController.swift
+++ iOS10-HW12-Alexandr-Ivanov/Sources/ViewController.swift
@@ -26,9 +26,9 @@ class ViewController: UIViewController {
     private lazy var timeLabel: UILabel = {
         let label = UILabel()
         label.text = "00:00"
-        label.font = UIFont.systemFont(ofSize: 60, weight: .thin)
+        label.font = UIFont.monospacedDigitSystemFont(ofSize: 60, weight: .thin)
         label.textColor = .white
-        label.textAlignment = .left
+        label.textAlignment = .center
         label.translatesAutoresizingMaskIntoConstraints = false
 
         return label
@@ -52,6 +52,17 @@ class ViewController: UIViewController {
         return view
     }()
 
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 60, weight: .thin)
+        label.textColor = .white
+        label.textAlignment = .center
+        label.text = "WORK"
+        label.translatesAutoresizingMaskIntoConstraints = false
+
+        return label
+    }()
+
     // MARK: - Life Cycle
 
     override func viewDidLoad() {
@@ -70,19 +81,23 @@ class ViewController: UIViewController {
         view.addSubview(circularProgressBarView)
         view.addSubview(timeLabel)
         view.addSubview(startStopButton)
+        view.addSubview(titleLabel)
     }
 
     private func setupLayout() {
         NSLayoutConstraint.activate([
             circularProgressBarView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             circularProgressBarView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            timeLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 1),
-            timeLabel.widthAnchor.constraint(equalToConstant: 156),
+            timeLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20),
+            timeLabel.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20),
             timeLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             startStopButton.heightAnchor.constraint(equalToConstant: 45),
             startStopButton.widthAnchor.constraint(equalToConstant: 45),
-            startStopButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: (((view.bounds.height / 2) + 60) / 2)),
+            startStopButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: view.bounds.height / 3.5),
             startStopButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleLabel.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 20),
+            titleLabel.rightAnchor.constraint(equalTo: view.rightAnchor, constant: -20),
+            titleLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -view.bounds.height / 3.5)
         ])
     }
 
@@ -116,10 +131,12 @@ class ViewController: UIViewController {
             duration = 10
             isWorkTime.toggle()
             runCount = 0
+            titleLabel.text = "REST"
         } else if rounded == 10.0 && !isWorkTime {
             duration = 25
             isWorkTime.toggle()
             runCount = 0
+            titleLabel.text = "WORK"
         } else if rounded == 0.01 {
             circularProgressBarView.progressAnimation(duration: duration)
         }
